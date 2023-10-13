16a9dc30ec157d6e419e9fa168ceb626ecc095e7
Piotr Kantorowicz <pkantorowicz@proton.me>
2023-10-13 18:13:09 +0200

    feat: added base specification classes
    Signed-off-by: Piotr Kantorowicz <pkantorowicz@proton.me>

---
+++ Src/Core/EssentialFrame/Specifications/AndSpecification.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EssentialFrame.Specifications;
+
+public class AndSpecification<T> : Specification<T>
+{
+    private readonly Specification<T>[] _specifications;
+
+    public AndSpecification(params Specification<T>[] specifications)
+    {
+        _specifications = specifications ?? throw new ArgumentNullException(nameof(specifications));
+    }
+
+    protected override Expression<Func<T, bool>> AsPredicateExpression()
+    {
+        Expression<Func<T, bool>> resultingExpression = null;
+
+        foreach (Specification<T> specification in _specifications)
+        {
+            if (resultingExpression is null)
+            {
+                resultingExpression = specification;
+                continue;
+            }
+
+            resultingExpression = Combine(resultingExpression, specification);
+        }
+
+        return resultingExpression;
+    }
+
+    private static Expression<Func<T, bool>> Combine(Expression<Func<T, bool>> leftExpression,
+        Expression<Func<T, bool>> rightExpression)
+    {
+        ParameterExpression parameter = Expression.Parameter(typeof(T));
+
+        ReplaceExpressionVisitor leftVisitor = new ReplaceExpressionVisitor(leftExpression.Parameters[0], parameter);
+        Expression left = leftVisitor.Visit(leftExpression.Body);
+
+        ReplaceExpressionVisitor rightVisitor = new ReplaceExpressionVisitor(rightExpression.Parameters[0], parameter);
+        Expression right = rightVisitor.Visit(rightExpression.Body);
+
+        if (left is null || right is null)
+        {
+            throw new InvalidOperationException("Left or right expression cannot be null.");
+        }
+
+        return Expression.Lambda<Func<T, bool>>(Expression.And(left, right), parameter);
+    }
+}
---
+++ Src/Core/EssentialFrame/Specifications/ReplaceExpressionVisitor.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace EssentialFrame.Specifications;
+
+public class ReplaceExpressionVisitor : ExpressionVisitor
+{
+    private readonly Expression _oldValue;
+    private readonly Expression _newValue;
+
+    public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+    {
+        _oldValue = oldValue;
+        _newValue = newValue;
+    }
+
+    public override Expression Visit(Expression node)
+    {
+        if (node == _oldValue)
+        {
+            return _newValue;
+        }
+
+        return base.Visit(node);
+    }
+}
